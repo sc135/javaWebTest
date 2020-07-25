9802a41cb1b7d9ce0059e5530a8bb45a9ac1d076
“Your <498575818@qq.com>
2020-07-25 13:05:33 +0800

    提交代码

---
+++ src/com/sc/bean/DebuggerTest.java
@@ -0,0 +1,22 @@
+package com.sc.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * desc
+ *
+ * @author sc135
+ * @date 2020-07-25 12:43  星期六
+ */
+public class DebuggerTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);list.add(9);list.add(8);
+        list.add(2);list.add(5);list.add(6);
+
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    }
+}
